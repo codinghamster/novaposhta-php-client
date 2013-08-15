aa3c50980e967138973abc89e41af458faf95f92
Hamster <admin@codinghamster.info>
2013-08-15 16:25:03 +0300

    add caching, bugfixes, delivery status, docs and examples

--- Novaposhta.php
+++ Novaposhta.php
@@ -8,6 +8,8 @@ class Novaposhta {
     
     const URL_CLOSEST_OFFICES = 'http://maps.novaposhta.ua/index.php?r=site/nearest';
     
+    const URL_DELIVERY_STATUS = 'http://novaposhta.ua/frontend/tracking/ru';
+    
     protected $baseOfficeLink = 'http://novaposhta.ua/map/warehouse/id/%s';
     
     protected $baseOfficeMap = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false&language=ru&region=ua';
@@ -15,6 +17,16 @@ class Novaposhta {
     protected $regexOfficeId = '#/map/warehouse/id/([0-9]+)#';
     
     protected $regexOfficeCity = '#(.+) \((.+)\)#';
+    
+    protected $regexOfficeWeights = '#(\(.*\))#';
+    
+    protected $uselessInAddress = array('Отделение,');
+    
+    protected $encoding = 'UTF-8';
+    
+    protected $cacheFile;
+    
+    protected $cacheExpire;
     
     private $_allOffices = null;
     
@@ -24,7 +36,36 @@ class Novaposhta {
         CURLOPT_TIMEOUT           => 60
     );
     
+    public function __construct($config = array()) {
+        $this->cacheFile = isset($config['cacheFile']) ? $config['cacheFile'] : dirname(__FILE__).'/.np_offices_cache';
+        $this->cacheExpire = isset($config['cacheExpire']) ? $config['cacheExpire'] : 24 * 3600;
+    }
+    
+    protected function _ucfirst($str) {
+        return mb_strtoupper(mb_substr($str, 0, 1, $this->encoding), $this->encoding).mb_substr($str, 1, mb_strlen($str, $this->encoding) - 1, $this->encoding);
+    }
+    
+    protected function _setCache($data) {
+        $f = @fopen($this->cacheFile, 'w');
+        if ($f !== false) {
+            fwrite($f, serialize($data));
+            fclose($f);
+        }
+    }
+    
+    protected function _getFromCache() {
+        if (!file_exists($this->cacheFile) || time() - filemtime($this->cacheFile) > $this->cacheExpire) {
+            return null;
+        }
+        return unserialize(file_get_contents($this->cacheFile));
+    }
+    
     public function getAllOffices() {
+        if ($this->_allOffices) {
+            return $this->_allOffices;
+        }
+        
+        $this->_allOffices = $this->_getFromCache();
         if ($this->_allOffices) {
             return $this->_allOffices;
         }
@@ -56,17 +97,28 @@ class Novaposhta {
                 preg_match($this->regexOfficeId, $a->getAttribute('href'), $match);
                 $offId = $match[1];
                 
-                $addrNode = $children->item(1);
-                $address = explode(':', trim($addrNode->nodeValue));
+                $offNode = $children->item(0);
+                $offName = $this->_ucfirst(trim($offNode->nodeValue));
                 
-                $offName = isset($address[0]) ? $address[0] : '';
-                $offAddress = isset($address[1]) ? $address[1] : '';
+                $addrNode = $children->item(1);
+                $address = explode(':', trim($addrNode->nodeValue), 2);
+                
+                if (count($address) < 2) {
+                    $offAddress = $address[0];
+                } else {
+                    if (preg_match($this->regexOfficeWeights, $address[0], $match)) {
+                        $offName .= ' '.$match[1];
+                    }
+                    $offAddress = $address[1];
+                }
+                $offAddress = trim(str_replace($this->uselessInAddress, '', $offAddress));
                 
                 $phoneNode = $children->item(2);
                 $offPhone = trim($phoneNode->nodeValue);
                 
                 $allOffices[] = array(
                     'id' => $offId,
+                    'link' => $this->getOfficeLink($offId),
                     'name' => $offName,
                     'address' => $offAddress,
                     'phone' => $offPhone,
@@ -76,6 +128,7 @@ class Novaposhta {
             }
         }
         
+        $this->_setCache($allOffices);
         $this->_allOffices = $allOffices;
         
         return $allOffices;
@@ -90,9 +143,9 @@ class Novaposhta {
         
         $url = sprintf($this->baseOfficeMap, $encoded);
         
-        $json_str = $this->_fetch($url);
+        $jsonStr = $this->_fetch($url);
         
-        $json = json_decode($json_str);
+        $json = json_decode($jsonStr);
         
         if ($json->status != 'OK') {
             return false;
@@ -113,14 +166,14 @@ class Novaposhta {
             return false;
         }
         
-        $json_str = $this->_fetch(self::URL_CLOSEST_OFFICES, $coords, true);
+        $jsonStr = $this->_fetch(self::URL_CLOSEST_OFFICES, $coords, true);
         
-        $json = json_decode($json_str);
+        $json = json_decode($jsonStr);
         
         $result = array();
         
         foreach($json as $office) {
-            $result[] = $office->id;
+            $result[] = $this->getOfficeById($office->id);
         }
         
         return $result;
@@ -177,7 +230,7 @@ class Novaposhta {
     public function getOfficesBy($field, $value, $substr = false) {
         $allOffices = $this->getAllOffices();
         
-        if (!isset($allOffices[0], $field)) {
+        if (!isset($allOffices[0][$field])) {
             throw new Novaposhta_Exception('Unknown field specified.');
         }
         
@@ -191,7 +244,7 @@ class Novaposhta {
         
         if ($substr) {
             foreach($allOffices as $office) {
-                if (mb_strpos($office[$field], $value, 0, 'uft-8') !== false) {
+                if (mb_strpos($office[$field], $value, 0, $this->encoding) !== false) {
                     $result[] = $office;
                 }
             }
@@ -201,6 +254,47 @@ class Novaposhta {
                     $result[] = $office;
                 }
             }
+        }
+        
+        return $result;
+    }
+    
+    public function getDeliveryStatus($id) {
+        $doc = @DOMDocument::loadHTML($this->_fetch(self::URL_DELIVERY_STATUS, array('en' => $id), true));
+        
+        $xpath = new DOMXPath($doc);
+        
+        $resultNode = $xpath->query('//div[@class="result"]/table|//div[@class="result"]/div');
+        
+        if ($resultNode->length != 1) {
+            return false;
+        }
+        
+        $statusNode = $resultNode->item(0);
+        
+        if ($statusNode->nodeName == 'table') {
+            $result = array();
+            
+            foreach($statusNode->getElementsByTagName('tr') as $tr) {
+                $cells = $tr->getElementsByTagName('td');
+                
+                $row = array(
+                    'caption' => $cells->item(0)->nodeValue,
+                    'message' => $cells->item(1)->nodeValue
+                );
+                
+                $a = $cells->item(1)->getElementsByTagName('a');
+                
+                if ($a->length) {
+                    if (preg_match($this->regexOfficeId, $a->item(0)->getAttribute('href'), $match)) {
+                        $row['ref_office'] = $this->getOfficeById($match[1]);
+                    }
+                }
+                
+                $result[] = $row;
+            }
+        } else {
+            $result = $statusNode->nodeValue;
         }
         
         return $result;
--- example.php
+++ example.php
@@ -1,7 +1,70 @@
 <?php
 
+function print_table($offices) {
+    echo '<table border="1">';
+        echo '<tr>';
+            echo '<th>ID</th>';
+            echo '<th>Город</th>';
+            echo '<th>Область</th>';
+            echo '<th>Ссылка</th>';
+            echo '<th>Адрес</th>';
+            echo '<th>Телефон</th>';
+        echo '</tr>';
+        foreach($offices as $office) {
+            echo '<tr>';
+                echo '<td>'.$office['id'].'</td>';
+                echo '<td>'.$office['city'].'</td>';
+                echo '<td>'.$office['region'].'</td>';
+                echo '<td><a href="'.$office['link'].'">'.$office['name'].'</a></td>';
+                echo '<td>'.$office['address'].'</td>';
+                echo '<td>'.$office['phone'].'</td>';
+            echo '</tr>';
+        }
+    echo '</table>';
+}
+
+function print_status($status) {
+    if (is_array($status)) {
+        echo '<table border="1">';
+            foreach($status as $row) {
+                echo '<tr>';
+                    echo '<th>'.$row['caption'].'</th>';
+                    echo '<td>'.$row['message'];
+                    
+                    if (isset($row['ref_office'])) {
+                        echo ' <a href="'.$row['ref_office']['link'].'">Карта</a>';
+                    }
+                    echo '</td>';
+                echo '</tr>';
+            }
+        echo '</table>';
+    } else {
+        echo $status;
+    }
+}
+
 require 'Novaposhta.php';
 
 $NP = new Novaposhta();
 
-var_dump($NP->getAllOffices());
+//Все офисы в Киеве
+$kievOffices = $NP->getOfficesBy('city', "Киев");
+echo '<h2>Все офисы в Киеве</h2>';
+print_table($kievOffices);
+
+//Все офисы в киевской области
+$kievRegionOffices = $NP->getOfficesBy('region', "Киевск", true);
+echo '<h2>Все офисы в киевской области</h2>';
+print_table($kievRegionOffices);
+
+//Ближайшие офисы
+$address = 'Киев, Хрещатик';
+$closestOffices = $NP->getClosestOffices($address);
+echo '<h2>Ближайшие офисы к "'.$address.'"</h2>';
+print_table($closestOffices);
+
+//Статус доставки
+$ttn = '12345678901234';
+$deliveryStatus = $NP->getDeliveryStatus($ttn);
+echo '<h2>Статус доставки с номером накладной №'.$ttn.'</h2>';
+print_status($deliveryStatus);
